daa277cd0db08d0cd0e9e915a3a5677ef6a4aba9
Egor Golikov <Kimepl@github.com>
2020-12-11 18:24:23 +0300

    add second fun add and two fun sub

--- ComplexNumber.c
+++ ComplexNumber.c
@@ -6,3 +6,22 @@ ComplexNumber add(ComplexNumber first, ComplexNumber second) {
 	result.re = first.im + second.im;
 	return result;
 }
+
+ComplexNumber add(ComplexNumber first, double second) {
+	ComplexNumber result;
+	result.re = first.re + second;
+	return result;
+}
+
+ComplexNumber sub(ComplexNumber first, ComplexNumber second) {
+	ComplexNumber result;
+	result.re = first.re - second.re;
+	result.im = first.im - second.im;
+	return result;
+}
+
+ComplexNumber sub(ComplexNumber first, double second) {
+	ComplexNumber result;
+	result.re = first.re - second;
+	return result;
+}
