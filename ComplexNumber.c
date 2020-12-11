8dc1d56d3e1b1c397c9d7e363bb53f5392d54aa6
Jark255 <jaroslav.asanov@gmail.com>
2020-12-11 18:08:38 +0300

    Implemented div functions

--- ComplexNumber.c
+++ ComplexNumber.c
@@ -6,3 +6,28 @@ ComplexNumber add(ComplexNumber first, ComplexNumber second) {
 	result.re = first.im + second.im;
 	return result;
 }
+
+ComplexNumber div(ComplexNumber first, ComplexNumber second) {
+	ComplexNumber result;
+
+	// Putting numbers as new variables for better reading
+	// This way first can be represented as (a + bi)
+	// And second -- (c + di)
+	double a = first.re;
+	double b = first.im;
+	double c = second.re;
+	double d = second.im;
+
+	result.re = (a*c + b*d) / (c*c + d*d);
+	result.im = (b*c - a*d) / (c*c + d*d);
+
+	return result;
+}
+
+ComplexNumber div(ComplexNumber first, double second) {
+	ComplexNumber result;
+	result.re = first.re / second;
+	result.im = first.im / second;
+
+	return result;
+}
